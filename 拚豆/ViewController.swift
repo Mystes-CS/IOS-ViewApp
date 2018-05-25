70621c596c98da75a56511a02a9cf6745d2dee95
Mystes-CS <mystes3016@gmail.com>
2018-05-25 20:39:36 +0800

    VIEWAPP

---
+++ 拚豆/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  拚豆
+//
+//  Created by Zonghan on 2018/3/22.
+//  Copyright © 2018年 Zonghan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
